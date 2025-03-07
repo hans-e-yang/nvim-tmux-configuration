328ed0052abdde0136181cef061ec0942c5225ec
hans <hansyang2005@gmail.com>
2025-03-07 19:48:29 +0800

    Removed empty files

--- .config/nvim/lua/hans/init.lua
+++ .config/nvim/lua/hans/init.lua
@@ -35,7 +35,6 @@ vim.api.nvim_create_autocmd('FileType', {
   callback = function()
     vim.lsp.start({
       name = "markdown lsp",
-      -- Custom md lsp server
       cmd = { os.getenv("HOME") .. "/.local/share/nvim/mdlsp" },
     })
   end
